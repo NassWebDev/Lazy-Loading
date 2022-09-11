a468c836739806861e41c38a926fbf7d6ceee61c
Nass <90240661+NassWebDev@users.noreply.github.com>
2022-09-12 00:10:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,53 @@
-const cards = [...document.querySelectorAll(".card")];
-const number = document.querySelector("span");
+const card = document.querySelectorAll(".card");
+let allcards = [...card];
 
-console.log(cards);
 
-const observer = new IntersectionObserver(
+let observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
-            if(entry.isIntersecting){
+            if(entry.intersectionRatio > 0.5){
                 entry.target.classList.add("card-lazy");
             }
-            
         })
     },
     {
-        threshold: 0.5
+        threshold:0.5
     }
 );
 
-const button = document.querySelector("button");
-console.log(button);
 
-const lastObserver = new IntersectionObserver(
-    (entries) => {
-        const lastCard = entries[0];
-        console.log(lastCard);
-        if(lastCard.intersectionRatio <= 0) return;
-        button.classList.add("active")
-        button.addEventListener("click", () => {
-            loadMore(10);
-            lastObserver.unobserve(lastCard.target);
-            lastObserver.observe(document.querySelector(".container"));
-        })
-        number.textContent = cards.length;
-    },
-    {}
-);
-
-lastObserver.observe(document.querySelector(".container"));
-
-cards.forEach(card => {
-    observer.observe(card);
-});
-
-
-const container = document.querySelector(".container");
-function loadMore(nbr){
-    let newCards = [];
-    for(let i = 0; i < nbr; i++){
+function loadCards(){
+    for(let i = 0; i < 10; i++){
         const acard = document.createElement("div");
         acard.classList.add("card");
         acard.textContent = "New Card";
         observer.observe(acard);
-        container.append(acard);
-        if(acard.isIntersecting){
-            acard.classList.add("card-lazy");
-        }
-        newCards.push(acard);
+        document.querySelector(".container").append(acard);
+        allcards.push(acard);
     }
-    cards.push.apply(cards, newCards);
 }
+
+const lastObserver = new IntersectionObserver(
+    (entries) => {
+        const lastCard = entries[0];
+        if(lastCard.intersectionRatio <= 0) return;
+        else if(lastCard.isIntersecting){
+            document.querySelector("button").classList.add("active");
+            document.querySelector("span").textContent = allcards.length;
+            document.querySelector("button").addEventListener("click", () => {
+                loadCards();
+                console.log(allcards.length);
+                document.querySelector("span").textContent = allcards.length;
+            })
+        }
+        // lastObserver.unobserve(lastCard.target);
+        lastObserver.observe(document.querySelector(".card:last-child"));
+    },
+    {}
+);
+
+
+allcards.forEach((cards) => {
+    observer.observe(cards);
+    lastObserver.observe(document.querySelector(".card:last-child"));
+})
